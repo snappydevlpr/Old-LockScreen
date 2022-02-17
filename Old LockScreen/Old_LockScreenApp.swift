963ee18d7814422126b4c17f2b7444889fdc97ec
Austin Suarez <austinmsuarez@gmail.com>
2022-02-16 21:41:32 -0600

    Initial Commit

---
+++ Old LockScreen/Old_LockScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Old_LockScreenApp.swift
+//  Old LockScreen
+//
+//  Created by Austin Suarez on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Old_LockScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
